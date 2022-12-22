b0c04d679346db37e5425164a3d8ac47243a8122
robinoldman <robin.c.oldman@gmail.com>
2022-12-22 17:07:42 +0000

    added js for creating a random word in an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 console.log ("connected")
+
+let guess = ['banana', 'apple', 'orange', 'satsuma', 'grape'];
+
+let randomGuess = Math.floor(Math.random() *guess.length);
+
+console.log(guess[randomGuess]);
