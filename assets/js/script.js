bc0a4293b79f7c6153790be036bb62855b68b52e
robinoldman <robin.c.oldman@gmail.com>
2022-12-27 17:00:41 +0000

    start word generation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,14 @@ console.log ("connected")
 let guess = ['banana', 'apple', 'orange', 'satsuma', 'grape'];
 
 let randomGuess = Math.floor(Math.random() *guess.length);
+let mamimumMistake = 6; 
+let guessed = [];
+
+document.getElementById('maximumMistake').innerHTML= mamimumMistake;
+
+function guessWord() {
+    wordStatus = randomGuess.split('').map(key ==> (guessed.indexOf(key)>=0?key: "_" )).joint('');
+    document.getElementsByClassName('word-container').innerHTML = wordStatus;
+}
 
 console.log(guess[randomGuess]);
