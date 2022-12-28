929d2b8d15106a9df5d1c7ad729ed24751b63ce1
robinoldman <robin.c.oldman@gmail.com>
2022-12-28 13:35:35 +0000

    change class keys to id key and make score card functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,50 +4,24 @@ var randomGuess = Math.floor(Math.random() * guess.length);
 //brings up _ _ _ for random guess
 var answerArray = [];
 for (var i = 0; i < guess[randomGuess].length; i++) {
-    answerArray[i] = "_"}
+    answerArray[i] = " _ "}
 
 
 var letterLeft = guess[randomGuess].length;
 document.getElementsByClassName("clue-container")[0].innerHTML = answerArray;
 
 
+//wrong guess score card
+var maximimumMistake = 6;
+document.getElementById('maximimumMistake').innerHTML = maximimumMistake
+
 console.log (answerArray);
 console.log(guess[randomGuess]);
 //main game 
 
-//while (lettersLeft > 0 ) {
-//alert(answerArray.join(' '));
+//while (letterLeft > 0 ) {
 
-//}
-
-
-
-//for (var i = 0; i < guess[randomGuess].length; i++) {
-   // if (playerGuesses.contains(guess[randomGuess].charAt(i))) {
-     //   console.log(guess[randomGuess].charAt(i));
-    //}
-   // else {
-     //   console.log("-");
-    //}
-//}
-
-//console.log(answerArray);
+  //var guess = prompt 
 
 
 
-//let mamimumMistake = 6; 
-//let guessed = [];
-//let wordStatus =null;
-
-
-
-//document.getElementById('maximumMistake').innerHTML= mamimumMistake;
-
-
-
-//function guessWord() {
-   // wordStatus= randomGuess.split('').map(letter => (guessed.indexOf(letter) >=0 ? letter: "_" )).joint('');
-    //document.getElementsByClassName('word-container').innerHTML = wordStatus;}
-
-
-//guessedWord():
