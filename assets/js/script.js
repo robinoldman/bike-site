81f4c231abe5ccf4e0d5231c2ae3ec48e308292f
robinoldman <robin.c.oldman@gmail.com>
2022-12-28 12:17:11 +0000

    add word length with array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,38 @@
-console.log ("connected")
+var guess = ['banana', 'apple', 'orange', 'satsuma', 'grape'];
 
-let guess = ['banana', 'apple', 'orange', 'satsuma', 'grape'];
+var randomGuess = Math.floor(Math.random() * guess.length);
 
-let randomGuess = Math.floor(Math.random() *guess.length);
-let mamimumMistake = 6; 
-let guessed = [];
-
-document.getElementById('maximumMistake').innerHTML= mamimumMistake;
-
-function guessWord() {
-    wordStatus = randomGuess.split('').map(key ==> (guessed.indexOf(key)>=0?key: "_" )).joint('');
-    document.getElementsByClassName('word-container').innerHTML = wordStatus;
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_"
 }
 
+//for (var i = 0; i < guess[randomGuess].length; i++) {
+   // if (playerGuesses.contains(guess[randomGuess].charAt(i))) {
+     //   console.log(guess[randomGuess].charAt(i));
+    //}
+   // else {
+     //   console.log("-");
+    //}
+//}
+
+//console.log(answerArray);
 console.log(guess[randomGuess]);
+
+
+//let mamimumMistake = 6; 
+//let guessed = [];
+//let wordStatus =null;
+
+
+
+//document.getElementById('maximumMistake').innerHTML= mamimumMistake;
+
+
+
+//function guessWord() {
+   // wordStatus= randomGuess.split('').map(letter => (guessed.indexOf(letter) >=0 ? letter: "_" )).joint('');
+    //document.getElementsByClassName('word-container').innerHTML = wordStatus;}
+
+
+//guessedWord():
