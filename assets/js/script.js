c5e32a5b797b315bf1d7c4ecc93d02a317fa19ff
robinoldman <robin.c.oldman@gmail.com>
2023-01-09 18:12:18 +0000

    add comments to css and html and format css and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,27 @@ const words = ['everest', 'bennevis', 'montblanc', 'matterhorn', 'snowdon', 'mou
 let answerArray = [];
 // selects random word
 let randomWordIndex = Math.floor(Math.random() * words.length);
-let index = 0; 
+let index = 0;
 const keyboardKeys = document.getElementsByClassName('key');
-let hangman = document.getElementById("hangman"); 
-  let hangmanImages = ["assets/images/tnt2.png","assets/images/tnt3.png",
-  "assets/images/tnt4.png","assets/images/tnt5.png","assets/images/tnt6.png",
-  "assets/images/tnt7.png","assets/images/explosion2.jpg",]; // Array of hangman images
+let hangman = document.getElementById("hangman");
+let hangmanImages = ["assets/images/tnt2.png", "assets/images/tnt3.png",
+    "assets/images/tnt4.png", "assets/images/tnt5.png", "assets/images/tnt6.png",
+    "assets/images/tnt7.png", "assets/images/explosion2.jpg",
+]; // Array of hangman images
 
 
 //event listener fo keyboard in game
 for (let key of keyboardKeys) {
-  key.addEventListener('click', function(event) {
-    let clickedKey = event.currentTarget;
+    key.addEventListener('click', function(event) {
+        let clickedKey = event.currentTarget;
 
-    key.classList.toggle('keypressed');
+        key.classList.toggle('keypressed');
 
-    let clickedLetter = clickedKey.dataset.value;
+        let clickedLetter = clickedKey.dataset.value;
 
-    guessLetter(clickedLetter);
-    checkGuesses();
-  });
+        guessLetter(clickedLetter);
+        checkGuesses();
+    });
 }
 
 
@@ -29,8 +30,8 @@ for (let key of keyboardKeys) {
 //refresh button - refreshes the whole game so that score goes back to 0
 const refreshButton = document.getElementById('refresh-button');
 refreshButton.addEventListener('click', function() {
-  reset();
-  location.reload();
+    reset();
+    location.reload();
 });
 
 
@@ -45,10 +46,8 @@ for (let i = 0; i < words[randomWordIndex].length; i++) {
 
 // sets the text content of the first element with the class 
 //"clue-container" to the elements of answerArray 
-let letterLeft = words[randomWordIndex].length;
+
 document.getElementsByClassName("clue-container")[0].innerHTML = answerArray.join('');
-
-
 
 
 
@@ -57,12 +56,12 @@ document.getElementsByClassName("clue-container")[0].innerHTML = answerArray.joi
 //selected word match the input letter. If there is a match, the corresponding 
 //index in the answerArray is updated to contain the matching letter. 
 function guessLetter(letter) {
-  for (let i = 0; i < answerArray.length; i++) {
-    if (words[randomWordIndex][i] === letter) {
-      
-      answerArray[i] = letter;
+    for (let i = 0; i < answerArray.length; i++) {
+        if (words[randomWordIndex][i] === letter) {
+
+            answerArray[i] = letter;
+        }
     }
-  }
 }
 
 
@@ -86,93 +85,94 @@ scoreElement.innerHTML = score;
 // if the letters all match and the word is guessed corrrectly the 'you won alert appears
 //if more than 7 wroing attemps have been made an error message appears sayin 'sorry you lost'
 function checkGuesses() {
-  const letters = words[randomWordIndex].split('');
-  let allLettersGuessed = true;
-  for (let i = 0; i < letters.length; i++) {
-    
-    if (!answerArray.includes(letters[i])) {
-      allLettersGuessed = false;
-      break;
-    }
-  }
-  if (allLettersGuessed) {
-    console.log('All the letters have been guessed');
-    score++;
-    localStorage.setItem('score', score);
-    scoreElement.innerHTML = score;
-    Swal.fire({
-      title: 'You won',
-      text: "You saved the mountain!",
-      icon: 'success',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'Next go',
-      
-    
-  }).then((result) => {
-    if (result.value) {
-      location.reload();
-    }
-  });
-  
-  }
+    const letters = words[randomWordIndex].split('');
+    let allLettersGuessed = true;
+    for (let i = 0; i < letters.length; i++) {
 
-  if (!allLettersGuessed)
-// taken from https://sweetalert2.github.io/
-if (wrong > 6) {
-  console.log('You lost');
-  setTimeout(function() {
-    Swal.fire({
-      title: 'You lost',
-      text: "Sorry you lost this time",
-      icon: 'error',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'Try again',
-      
-    }).then((result) => {
-      if (result.value) {
-        location.reload();
-      }
-    });
-  }, 2000);
-}}
+        if (!answerArray.includes(letters[i])) {
+            allLettersGuessed = false;
+            break;
+        }
+    }
+    if (allLettersGuessed) {
+        console.log('All the letters have been guessed');
+        score++;
+        localStorage.setItem('score', score);
+        scoreElement.innerHTML = score;
+        Swal.fire({
+            title: 'You won',
+            text: "You saved the mountain!",
+            icon: 'success',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Next go',
+
+
+        }).then((result) => {
+            if (result.value) {
+                location.reload();
+            }
+        });
+
+    }
+
+    if (!allLettersGuessed)
+        // taken from https://sweetalert2.github.io/
+        if (wrong > 6) {
+            console.log('You lost');
+            setTimeout(function() {
+                Swal.fire({
+                    title: 'You lost',
+                    text: "Sorry you lost this time",
+                    icon: 'error',
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText: 'Try again',
+
+                }).then((result) => {
+                    if (result.value) {
+                        location.reload();
+                    }
+                });
+            }, 2000);
+        }
+}
 
 
 
 
 //checks if the letter that was guessed is correct If there is a match, the value of isCorrect is set to true.
 function guessLetter(letter) {
-  let isCorrect = false; 
+    let isCorrect = false;
 
-  for (let i = 0; i < answerArray.length; i++) {
-    if (words[randomWordIndex][i] === letter) {
-
-      
+    for (let i = 0; i < answerArray.length; i++) {
+        if (words[randomWordIndex][i] === letter) {
 
 
-// Update the answerArray to replace the dash with the correct letter
-      answerArray[i] = letter;
-      isCorrect = true;
+
+
+            // Update the answerArray to replace the dash with the correct letter
+            answerArray[i] = letter;
+            isCorrect = true;
+        }
     }
-  }
 
 
 
-  // Shows the new picture if wrong answer by going through the array of images
-  
-  document.getElementsByClassName("clue-container")[0].innerHTML = answerArray.join('');
-  
+    // Shows the new picture if wrong answer by going through the array of images
+
+    document.getElementsByClassName("clue-container")[0].innerHTML = answerArray.join('');
 
 
 
-//if a wrong letter the next picture adds one
-//if wrong letterthe wrong letter index goes up by 1
-  if (!isCorrect) {
-    hangman.src = hangmanImages[index];
-    index++; 
-    wrong++;
-    wrongElement.innerHTML = wrong;
-    
-  }
+
+    //if a wrong letter the next picture adds one
+    //if wrong letterthe wrong letter index goes up by 1
+    if (!isCorrect) {
+        hangman.src = hangmanImages[index];
+        index++;
+        wrong++;
+        wrongElement.innerHTML = wrong;
+
+    }
 }
 
 
@@ -183,9 +183,7 @@ function guessLetter(letter) {
 
 
 function reset() {
-  score= 0;
-  localStorage.setItem('score', 0);
-  document.getElementById('score').innerHTML = 0;
+    score = 0;
+    localStorage.setItem('score', 0);
+    document.getElementById('score').innerHTML = 0;
 }
-
-
